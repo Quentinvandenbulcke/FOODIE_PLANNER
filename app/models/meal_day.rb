4b70fd4295fbd1e8619cb5e37f500c5845991a2e
Quentin Vandenbulcke <vandenbulckequentin@gmail.com>
2023-06-05 17:52:58 +0200

    ingredient table / migrate / validation ingr/mealday/recipe = ok

--- app/models/meal_day.rb
+++ app/models/meal_day.rb
@@ -1,4 +1,7 @@
 class MealDay < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
+
+  validates :date, presence: true
+  validates :quantity, presence: true, numericality: {{ only_integer: true, greater_than: 0 }}
 end
