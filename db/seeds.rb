a4665460d7238d2fe5903a168759a7777b8553b9
thomas allen <thomasallenmartinho@gmail.com>
2023-06-05 19:26:22 +0200

    end of day commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# puts "let's populate the recipe database !"
+# puts "."
+# puts "1"
+# puts "."
+# puts "2"
+# puts "."
+# puts "3"
+# puts "."
+# puts "let's gooo"
+
+require "json"
+require "open-uri"
+
+app_id = "2df0c1e7"
+app_key = "d8f7fc5422fbb9639eab44868599c0a6"
+
+
+#https://api.edamam.com/api/recipes/v2?type=public&q=pasta&app_id=2df0c1e7&app_key=d8f7fc5422fbb9639eab44868599c0a6
+
+
+url = "https://api.edamam.com/api/recipes/v2?type=public&q=pasta&app_id=2df0c1e7&app_key=d8f7fc5422fbb9639eab44868599c0a6"
+#url = "https://api.edamam.com/api/recipes/v2/?&app_id=#{app_id}&app_key=#{app_key}"
+file = URI.open(url).read
+data = JSON.parse(file)
+
+puts "#{data}"
