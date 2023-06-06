30c5822bb30a31d0ab85dd5f2c31b6aa9c6fd537
thomas allen <thomasallenmartinho@gmail.com>
2023-06-06 15:53:02 +0200

    every recipe has a photo yay

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :meal_days
   has_many :ingredients
-
+  has_one_attached :photo
   validates :name, :description, presence: true
   validates :prep_time, :rating, :servings, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
