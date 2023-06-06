0383e5b70c243645d0197820e6f5f99163c280b0
Quentin Vandenbulcke <vandenbulckequentin@gmail.com>
2023-06-06 15:26:41 +0200

    still working

--- app/controllers/meal_days_controller.rb
+++ app/controllers/meal_days_controller.rb
@@ -1,9 +1,11 @@
 class MealDaysController < ApplicationController
   def index
+    @meal_days = policy_scope(MealDay)
   end
 
   def new
     @meal_day = MealDay.new
     @recipe = Recipe.find(params[:recipe_id])
+    authorize @meal_day
   end
 end
