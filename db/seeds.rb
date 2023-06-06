ba051c8aac73a179536a3e1b6c865cc6b0926c02
thomas allen <thomasallenmartinho@gmail.com>
2023-06-06 14:36:40 +0200

    done with dbsss

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,6 @@
 require "json"
 require "open-uri"
 
-app_id = "2df0c1e7"
-app_key = "d8f7fc5422fbb9639eab44868599c0a6"
-
 cuisine_types = [
   "Mexican",
   "Asian",
@@ -20,7 +17,7 @@ cuisine_types = [
 puts "let's populate the databases !"
 
 cuisine_types.each do |type|
-  url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{app_id}&app_key=#{app_key}&cuisineType=#{type}"
+  url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{EDAMAN_APP_ID}&app_key=#{EDAMAN_APP_KEY}&cuisineType=#{type}"
   url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=2df0c1e7&app_key=d8f7fc5422fbb9639eab44868599c0a6&cuisineType=Mexican"
   file = URI.open(url).read
   data = JSON.parse(file)
