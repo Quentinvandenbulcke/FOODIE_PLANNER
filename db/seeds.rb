8caa3e07a864a6630d2005de6886f0754407194d
thomas allen <thomasallenmartinho@gmail.com>
2023-06-06 14:19:19 +0200

    db populated yayy

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,25 +21,30 @@ puts "let's populate the databases !"
 
 cuisine_types.each do |type|
   url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{app_id}&app_key=#{app_key}&cuisineType=#{type}"
+  url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=2df0c1e7&app_key=d8f7fc5422fbb9639eab44868599c0a6&cuisineType=Mexican"
   file = URI.open(url).read
   data = JSON.parse(file)
+  puts "starting with #{type} recipes"
 
   data["hits"].each do |hit|
-    recipe = Recipe.new(
+    recipe = Recipe.create!(
       name: hit["recipe"]["label"],
       description: hit["recipe"]["ingredientLines"].join("\n"),
-      prep_time: hit["recipe"]["totalTime"].to_i,
+      prep_time: hit["recipe"]["totalTime"].to_i > 0 ? hit["recipe"]["totalTime"].to_i : [15, 30, 45, 60].sample,
       rating: (1..5).to_a.sample,
       servings: hit["recipe"]["yield"].to_i,
       category: type
-    ).save!
+    )
+    puts "creating ingredients for #{recipe.name}"
     hit["recipe"]["ingredients"].each do |ingredient|
-      Ingredient.new(
+      Ingredient.create!(
         name: ingredient["food"],
         quantity: ingredient["quantity"],
-        unit: ingredient["measure"],
+        unit: ingredient["measure"].blank? ? "N.A" : ingredient["measure"],
         recipe_id: recipe.id
-      ).save!
+      )
     end
   end
 end
+
+puts "done!"
