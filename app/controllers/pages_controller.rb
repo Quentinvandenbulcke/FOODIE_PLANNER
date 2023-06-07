2566fba16b6a5e02aa07214baf9b9e0a4a4420fe
thomas allen <thomasallenmartinho@gmail.com>
2023-06-07 12:29:12 +0200

    getting there calendar looking kinda good

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
     @recipes = policy_scope(Recipe)
+    @meal_days = []
+    # @meal_days = MealDay.where(
+    #   starts_at: date.beginning_of_month.beginning_of_week..date.end_of_month.end_of_week
+    # )
   end
 end
