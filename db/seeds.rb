c39576187e9fe998cda6a5817c79e8abf841f6b3
thomas allen <thomasallenmartinho@gmail.com>
2023-06-08 10:28:45 +0200

    starting favorite recipes model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require "json"
+require "open-uri"
+require 'faker'
+
 puts "Destroying Users"
 User.destroy_all
 puts "Destroying grocery list"
@@ -25,8 +29,6 @@ cuisine_types = [
   "Mediterranean"
 ]
 
-require 'faker'
-
 names = [
   "quentin",
   "gab",
@@ -44,56 +46,54 @@ names.each do |name|
   )
   puts "|"
 end
+
 puts "done with users!"
 
 puts "Creating empty grocery to initialize mealdays"
 Grocery.create(user: User.first)
 
-# require "json"
-# require "open-uri"
+cuisine_types = [
+  "Mexican",
+  "Asian",
+  "British",
+  "Caribbean",
+  "Chinese",
+  "Indian",
+  "Italian",
+  "Nordic",
+  "Mediterranean"
+]
 
-# cuisine_types = [
-#   "Mexican",
-#   "Asian",
-#   "British",
-#   "Caribbean",
-#   "Chinese",
-#   "Indian",
-#   "Italian",
-#   "Nordic",
-#   "Mediterranean"
-# ]
+puts "let's populate the databases !"
 
-# puts "let's populate the databases !"
+cuisine_types.each do |type|
+  url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{ENV.fetch("EDAMAN_APP_ID")}&app_key=#{ENV.fetch("EDAMAN_APP_KEY")}&cuisineType=#{type}&mealType=Dinner"
+  file = URI.open(url).read
+  data = JSON.parse(file)
+  puts "starting with #{type} recipes"
 
-# cuisine_types.each do |type|
-#   url = "https://api.edamam.com/api/recipes/v2?type=public&app_id=#{ENV.fetch("EDAMAN_APP_ID")}&app_key=#{ENV.fetch("EDAMAN_APP_KEY")}&cuisineType=#{type}&mealType=Dinner"
-#   file = URI.open(url).read
-#   data = JSON.parse(file)
-#   puts "starting with #{type} recipes"
+  data["hits"][0..4].each do |hit|
+    photo = URI.open(hit["recipe"]["image"])
+    recipe = Recipe.new(
+      name: hit["recipe"]["label"],
+      description: hit["recipe"]["ingredientLines"].join("\n"),
+      prep_time: hit["recipe"]["totalTime"].to_i > 0 ? hit["recipe"]["totalTime"].to_i : [15, 30, 45, 60].sample,
+      rating: (1..5).to_a.sample,
+      servings: hit["recipe"]["yield"].to_i,
+      category: type
+    )
+    recipe.photo.attach(io: photo, filename: "#{recipe.name.split().join("_")}.png", content_type: "image/png")
+    recipe.save!
+    puts "creating ingredients for #{recipe.name}"
+    hit["recipe"]["ingredients"].each do |ingredient|
+      Ingredient.create!(
+        name: ingredient["food"],
+        quantity: ingredient["quantity"],
+        unit: ingredient["measure"].blank? ? "N.A" : ingredient["measure"],
+        recipe_id: recipe.id
+      )
+    end
+  end
+end
 
-#   data["hits"][0..4].each do |hit|
-#     photo = URI.open(hit["recipe"]["image"])
-#     recipe = Recipe.new(
-#       name: hit["recipe"]["label"],
-#       description: hit["recipe"]["ingredientLines"].join("\n"),
-#       prep_time: hit["recipe"]["totalTime"].to_i > 0 ? hit["recipe"]["totalTime"].to_i : [15, 30, 45, 60].sample,
-#       rating: (1..5).to_a.sample,
-#       servings: hit["recipe"]["yield"].to_i,
-#       category: type
-#     )
-#     recipe.photo.attach(io: photo, filename: "#{recipe.name.split().join("_")}.png", content_type: "image/png")
-#     recipe.save!
-#     puts "creating ingredients for #{recipe.name}"
-#     hit["recipe"]["ingredients"].each do |ingredient|
-#       Ingredient.create!(
-#         name: ingredient["food"],
-#         quantity: ingredient["quantity"],
-#         unit: ingredient["measure"].blank? ? "N.A" : ingredient["measure"],
-#         recipe_id: recipe.id
-#       )
-#     end
-#   end
-# end
-
-# puts "done!"
+puts "done!"
