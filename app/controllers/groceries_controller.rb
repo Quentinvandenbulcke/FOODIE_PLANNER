f0b208b20b26ac0bef78db04522348c654c549e4
Quentin Vandenbulcke <vandenbulckequentin@gmail.com>
2023-06-08 10:04:00 +0200

    groceries ok - check with test

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -10,14 +10,16 @@ class GroceriesController < ApplicationController
 
   def show
     @grocery = Grocery.find(params[:id])
-    @lists = GroceryList.select {|list| list.grocery_id = @grocery.id }
-    @meals = MealDay.select {|meal| meal.grocery_id = @grocery.id }
+    @lists = GroceryList.select {|list| list.grocery_id == @grocery.id }
+    @meals = MealDay.select {|meal| meal.grocery_id == @grocery.id }
     authorize @grocery
   end
 
   def create
     array_of_selected_meals = params[:grocery][:meal_day_ids]
     @grocery = Grocery.create(user: current_user)
+    quantities = Hash.new(0)
+
     array_of_selected_meals.each do |meal|
       if !meal.empty?
         @meal = MealDay.find(meal.to_i)
@@ -26,30 +28,63 @@ class GroceriesController < ApplicationController
           recipe_servings = @meal.recipe.servings
           meal_servings = @meal.quantity
           ingredient_quantity = ((recipe_ingredient_qty / recipe_servings) * meal_servings).round(2)
-          @grocery_list = GroceryList.new
-          @grocery_list.grocery_id = @meal.grocery_id
-          @grocery_list.ingredient = ingredient
-          @grocery_list.quantity = ingredient_quantity
-          @grocery_list.grocery = @grocery
-          unless @grocery_list.save
-            render :new, status: :unprocessable_entity
-          end
-          authorize @grocery_list
+          quantities[ingredient.name] += ingredient_quantity
         end
       end
     end
+    quantities.each do |ingredient_name, quantity|
+      ingredient = Ingredient.find_by(name: ingredient_name)
+      @grocery_list = GroceryList.new(
+        grocery_id: @grocery.id,
+        ingredient: ingredient,
+        quantity: quantity
+      )
+      unless @grocery_list.save
+        render :new, status: :unprocessable_entity
+        return
+      end
+      authorize @grocery_list
+    end
     authorize @grocery
-    redirect_to groceries_new_path
+    redirect_to grocery_path(@grocery)
   end
+
 end
 
 
-# <% current_user.meal_days.each do |meal| %>
-#   <% meal.recipe.ingredients.each do |ingredient| %>
-#     <% ingredient_quantity = BigDecimal(ingredient.quantity)%>
-#     <% recipe_servings = BigDecimal(meal.recipe.servings) %>
-#     <% meal_quantity = BigDecimal(meal.quantity) %>
-#     <div class="grocery-row">
-#       <div class="grocery-cell"><%= ingredient.name %></div>
-#       <div class="grocery-cell grocery-center"><%= ((ingredient_quantity / recipe_servings) * meal_quantity).round(2) %></div>
-#       <div class="grocery-cell grocery-center"><%= ingredient.unit %></div>
+# def create
+#   array_of_selected_meals = params[:grocery][:meal_day_ids]
+#   @grocery = Grocery.create(user: current_user)
+#   quantities = Hash.new(0)
+
+#   array_of_selected_meals.each do |meal|
+#     if !meal.empty?
+#       @meal = MealDay.find(meal.to_i)
+#       @meal.recipe.ingredients.each do |ingredient|
+#         recipe_ingredient_qty = BigDecimal(ingredient.quantity)
+#         recipe_servings = @meal.recipe.servings
+#         meal_servings = @meal.quantity
+#         ingredient_quantity = ((recipe_ingredient_qty / recipe_servings) * meal_servings).round(2)
+
+#         quantities[ingredient] += ingredient_quantity
+#       end
+#     end
+#   end
+
+#   quantities.each do |ingredient, quantity|
+#     @grocery_list = GroceryList.new(
+#       grocery_id: @meal.grocery_id,
+#       ingredient: ingredient,
+#       quantity: quantity,
+#       grocery: @grocery
+#     )
+#     unless @grocery_list.save
+#       render :new, status: :unprocessable_entity
+#       return
+#     end
+#     authorize @grocery_list
+#   end
+
+#   authorize @grocery
+#   redirect_to groceries_new_path
+# end
