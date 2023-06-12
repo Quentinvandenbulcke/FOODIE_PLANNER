cfc7301a37fbe9300c8a633f3493bbd9becc7b50
thomas allen <thomasallenmartinho@gmail.com>
2023-06-12 15:13:08 +0200

    select days solved - beautifuk

--- app/javascript/controllers/show_check_box_controller.js
+++ app/javascript/controllers/show_check_box_controller.js
@@ -1,24 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
+import { end } from "@popperjs/core";
 
 // Connects to data-controller="show-check-box"
 export default class extends Controller {
 
-  static targets = ["select", "generate", "checkbox"]
+  static targets = ["select", "generate", "checkbox", "submit", "tick"]
 
   connect() {
     console.log("Connected to show-checkbox-controller")
   }
 
   show() {
-    // console.log(this.selectTarget)
-    // console.log(this.generateTarget)
-    console.log(this.checkboxTarget);
-    console.log(this.checkboxTargets);
     this.generateTarget.classList.remove("cal-hide");
     this.checkboxTargets.forEach(check => {
-      console.log(check)
       check.classList.remove("cal-hide");
     });
     this.selectTarget.classList.add("cal-hide");
+  }
+
+  enableGenerate() {
+    let foundChecked = false
+    this.tickTargets.forEach(tick => {
+      if (tick.checked) {
+        foundChecked = true
+      };
+
+      if (foundChecked == true) {
+        this.submitTarget.classList.remove("btn-disable");
+      } else {
+        this.submitTarget.classList.add("btn-disable");
+      };
+    });
   }
 }
