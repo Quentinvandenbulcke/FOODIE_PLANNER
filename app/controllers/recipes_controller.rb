d6deb8b38ae23e3df10283d75b0e0ba065950368
Quentin Vandenbulcke <vandenbulckequentin@gmail.com>
2023-06-15 13:16:42 +0200

    solved bug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :new, :create]
 
   def index
-    @recipes = policy_scope(Recipe).uniq { |recipe| recipe.name }
+    @recipes = policy_scope(Recipe)
     @categories = []
     @recipes.each do |recipe|
       if recipe.name.include?(":")
